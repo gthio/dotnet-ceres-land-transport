07b052ac64028d0e89003830eacc3a212d1e9ebe
gthio <gunawanthio@outlook.com>
2016-04-28 17:55:04 +0800

    Initial commit

---
+++ 91 SrcTest/Test.Ceres.Runner/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Ceres.Gateway;
+
+namespace Test.Ceres.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
