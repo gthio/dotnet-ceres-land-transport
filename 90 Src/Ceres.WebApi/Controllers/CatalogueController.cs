0856656cb300324e1f42dc23823797635a851a5f
gthio <gunawanthio@outlook.com>
2016-07-21 21:57:50 +0800

    woof

--- 90 Src/Ceres.WebApi/Controllers/CatalogueController.cs
+++ 90 Src/Ceres.WebApi/Controllers/CatalogueController.cs
@@ -11,13 +11,15 @@ using System.Threading.Tasks;
 
 namespace Ceres.WebApi.Controllers
 {
+    [RoutePrefix("api/BusStops")]
     public class CatalogueController : ApiController
     {
         public CatalogueController()
         {
         }
 
-        // GET api/values
+        [HttpGet]
+        [Route("")]
         public IEnumerable<DynamicEntity> Get()
         {
             return MySingleton.Instance.BusStops
@@ -31,6 +33,74 @@ namespace Ceres.WebApi.Controllers
 
                     return s;
                 });
+        }
+
+        [HttpGet]
+        [Route("Search")]
+        public IEnumerable<DynamicEntity> ListByRoadName(string roadName)
+        {
+            return MySingleton.Instance.BusStops
+                .Where(x => x.GetMemberAsString("RoadName").Contains(roadName))
+                .Select(x =>
+                {
+                    var s = new DynamicEntity();
+
+                    s.SetMember("BusStopCode", x.GetMemberAsString("BusStopCode"));
+                    s.SetMember("RoadName", x.GetMemberAsString("RoadName"));
+                    s.SetMember("Description", x.GetMemberAsString("Description"));
+
+                    return s;
+                });
+        }
+
+        [HttpGet]
+        [Route("Search")]
+        public IEnumerable<DynamicEntity> ListByRoadDescription(string roadDescription)
+        {
+            return MySingleton.Instance.BusStops
+                .Where(x => x.GetMemberAsString("Description").Contains(roadDescription))
+                .Select(x =>
+                {
+                    var s = new DynamicEntity();
+
+                    s.SetMember("BusStopCode", x.GetMemberAsString("BusStopCode"));
+                    s.SetMember("RoadName", x.GetMemberAsString("RoadName"));
+                    s.SetMember("Description", x.GetMemberAsString("Description"));
+
+                    return s;
+                });
+        }
+
+        [HttpGet]
+        [Route("Search")]
+        public IEnumerable<DynamicEntity> ListByGeo(decimal latitude, 
+            decimal longitude, 
+            decimal? radius = 1)
+        {
+            return new List<DynamicEntity>();
+        }
+
+        [HttpGet]
+        [Route("{busStopCode}")]
+        public DynamicEntity Get(string busStopCode)
+        {
+            if (MySingleton.Instance.BusStops.Count(x => x.GetMemberAsString("BusStopCode") == busStopCode) > 0)
+            {
+                var result = MySingleton.Instance.BusStops
+                    .Where(x => x.GetMemberAsString("BusStopCode") == busStopCode)
+                    .Take(1)
+                    .Single();
+
+                var s = new DynamicEntity();
+
+                s.SetMember("BusStopCode", result.GetMemberAsString("BusStopCode"));
+                s.SetMember("RoadName", result.GetMemberAsString("RoadName"));
+                s.SetMember("Description", result.GetMemberAsString("Description"));
+
+                return s;
+            }
+
+            return null;
         }
     }
 }
